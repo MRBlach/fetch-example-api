664a125ae5bdf661a075a48bed9a172c05ef91f0
Melody Blachowicz <melodyblachowicz@gmail.com>
2021-01-01 17:04:23 -0800

    updates

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const usersBtn = document.getElementById('users').addEventListener('click', getU
 const commentsBtn = document.getElementById('comments').addEventListener('click', getComments);
 const photosBtn = document.getElementById('photos').addEventListener('click', getPhotos);
 let outputData = document.getElementById('output');
-document.querySelector('.container').style.marginTop = '150px';
+document.querySelector('.card').style.marginTop = '150px';
 
 
 //function for users button event listener
@@ -22,7 +22,7 @@ async function getUsers(e) {
   outputData.innerHTML = output;
   document.querySelector('.users').hidden = false;
   document.querySelector('.photos').hidden = true;
-  document.querySelector('.container').style.marginTop = '0';
+  document.querySelector('.card').style.marginTop = '0';
 }
 
 
@@ -49,24 +49,10 @@ async function getComments(e) {
   outputData.innerHTML = output;
   document.querySelector('.users').hidden = false;
   document.querySelector('.photos').hidden = true;
-  document.querySelector('.container').style.marginTop = '0';
+  document.querySelector('.card').style.marginTop = '0';
  }
 
 //function for photos button event listener
-// function getPhotos(){
-//   fetch('https://aws.random.cat/meow')
-//     .then(response => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       let imageUrl = data.file;
-//       output.innerHTML = `<img src=${imageUrl}>`;
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       alert(error);
-//     });
-// }
-
  function getPhotos(){
   fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
@@ -76,49 +62,10 @@ async function getComments(e) {
       outputData.innerHTML = `<img class='mx-auto' src='${imageUrl}' style='display:block; width:100%'>`;
       document.querySelector('.users').hidden = true;
       document.querySelector('.photos').hidden = false;
-      document.querySelector('.container').style.marginTop = '25px';
+      document.querySelector('.card').style.marginTop = '25px';
     })
     .catch((error) => {
       console.log(error);
       alert(error);
     });
 }
-
-// function getPhotos(){
-//   fetch('https://jsonplaceholder.typicode.com/photos/5')
-//     .then(response => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       let imageUrl = data.url;
-//       output.innerHTML = `<img src=${imageUrl}>`;
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       alert(error);
-//     });
-// }
-
-// function getPhotos(){
-//   fetch('https://picsum.photos/200')
-//     .then(response => response)
-//     .then((data) => {
-//       console.log(data);
-//       let imageUrl = data.url;
-//       output.innerHTML = `<img src=${imageUrl}>`;
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       //alert(error);
-//     })
-    
-// }
-
-
-//https://aws.random.cat/meow  data.file
-//https://dog.ceo/api/breeds/image/random  data.message
-
-//https://jsonplaceholder.typicode.com/photos  
-//        data.url for single photo,
-//        some sort of forEach loop for all photos
-
-//https://picsum.photos/200  data.url
